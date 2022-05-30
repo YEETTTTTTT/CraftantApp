9e8cd9031270c5b1a353462e6092983ff22bfabd
YEETTTTTTT <51404150+YEETTTTTTT@users.noreply.github.com>
2022-05-30 16:58:19 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,124 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const validator = require('validator');
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: "OurLittleSecret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb+srv://Osehunxo:Fireheart02!@craftant.fe9ox.mongodb.net/userDB");
+
+const userSchema = new mongoose.Schema ({
+  username: {
+    type: String,
+    required: [true, "Username is required."],
+    unique: [true, "Username is taken."]
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    unique: [true, "Email Address is taken."],
+    validate: [validator.isEmail, "Enter a valid email address."]
+  },
+  password: String,
+  userType: {
+    type: String,
+    required: [true, "Enter a password"],
+    minLength: [6, "Password should be at least 6 characters."],
+    maxLength: [20, "Password should not be longer than 20 characters."]
+  },
+});
+
+//use to hash and salt passwords
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+//creates cookie
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+//check if user is already logged in
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets", {userType: req.user.userType});
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/", function(req, res) {
+  res.render("home");
+});
+
+app.post("/register", function(req, res) {
+  User.register({username: req.body.username, email: req.body.name, userType: req.body.select}, req.body.password, function(err, user){
+    if (err) {
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      })
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  })
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
+});
